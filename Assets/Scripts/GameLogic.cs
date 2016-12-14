cb2bbb8e243f0977b636759cd49135fb221f9bfa
Andy Despres <adespres@gmail.com>
2016-12-14 13:02:56 -0800

    Created new Unity project and imported packages
    After creating the new Unity project I imported the Google VR SDK and
    the puzzler starter package. These are the core packages required for
    this project.

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject startUI, restartUI;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void toggleUI() {
+		startUI.SetActive (!startUI.activeSelf);
+		restartUI.SetActive (!restartUI.activeSelf);
+	}
+}
