71b7406ff7349cbaa8a6b385e7645f2ce828d10c
a-despres <adespres@gmail.com>
2017-01-05 21:43:04 -0800

    Added motion mechanic
    Placed the start and restart UI panels in their final locations and
    implemented the motion mechanic for moving through the game.
    
    The motion of the camera utilizes a third-party plugin called iTween.
    This plugin simplifies the implementation of the motion mechanic. While
    this same motion could be accomplished by using a LERP, the plugin
    implementation takes three arguments: position, time, and easing —
    greatly simplifying the task.
    
    In user testing for the motion experience, both users that were tested
    said that the motion was too fast. One person said that the speed felt
    unnatural and the other said that it almost made them feel dizzy. At
    the time of testing the camera moved from the start UI panel to the
    game position in 2 seconds. It took the same amount of time to move
    from the game position to the restart UI panel. Following the tests,
    and prior to this commit, I decreased the speed so that it now takes 3
    seconds to move between each position.

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -5,18 +5,49 @@ public class GameLogic : MonoBehaviour {
 
 	public GameObject player;
 	public GameObject startUI, restartUI;
+	public GameObject startPoint, playPoint, restartPoint;
 
+	public bool playerWon = false;
 
 	// Use this for initialization
 	void Start () {
-
+		player.transform.position = startPoint.transform.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButtonDown (0) && player.transform.position==playPoint.transform.position) {
+			puzzleSuccess ();
+		}
+	}
+
+	public void startPuzzle() { //Begin the puzzle sequence
+		toggleUI();
+		iTween.MoveTo (player, 
+			iTween.Hash (
+				"position", playPoint.transform.position, 
+				"time", 3, 
+				"easetype", "linear"
+			)
+		);
 
 	}
 
+	public void resetPuzzle() { //Reset the puzzle sequence
+		player.transform.position = startPoint.transform.position;
+		toggleUI ();
+	}
+
+
+	public void puzzleSuccess() { //Do this when the player gets it right
+		iTween.MoveTo (player, 
+			iTween.Hash (
+				"position", restartPoint.transform.position, 
+				"time", 3, 
+				"easetype", "linear"
+			)
+		);
+	}
 	public void toggleUI() {
 		startUI.SetActive (!startUI.activeSelf);
 		restartUI.SetActive (!restartUI.activeSelf);
